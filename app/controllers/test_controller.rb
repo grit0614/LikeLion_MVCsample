572ee001716e2a40e5ab7bf2b7d5c954905b3703
grit0614 <grit0614@gmail.com>
2016-04-04 04:53:59 +0000

    Third Commit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -11,31 +11,38 @@ class TestController < ApplicationController
         @pickmeup = @food_list.sample
         @foodpic = @pickmeup + ".png"
         
-        # 중복값 제거한 로또 생성 (아직 안배워서 뭔소리인지 잘 모르겠다. 제끼자.)
-        def draw(num)
-            draw_arrays = Array.new
-            while draw_arrays.size < num
-                draw_arrays << (1..45).to_a.sample(6).sort
-            end
-            
-            draw_arrays.uniq!
+        # 코드라이언에 있던 답.....인데
+        # 있는데 써먹지를 못하겠어요.. 뭐하는건진 대충 알것같은데
+        # view로 던지는 법을 모르니 깔끔하게 포기! (찡긋)
+#        def draw(num)
+#            draw_arrays = Array.new
+#            while draw_arrays.size < num
+#                draw_arrays << (1..45).to_a.sample(6).sort
+#            end
+#            
+#            draw_arrays.uniq!
+#        
+#            draw_arrays.each do |array|
+#                puts array.inspect
+#            end
+#        end
         
-            draw_arrays.each do |array|
-                puts array.inspect
-            end
-        end
+        # 그냥 정말 노가다로 6개 찍어내자
+        draw_arrays = Array.new                                                 # array 생성
+        draw_arrays << (1..45).to_a.sample(6).sort                              # 1~45까지 랜덤 6개 추출, 순차적으로 정렬
+        draw_arrays.uniq!                                                       # 중복원소 제거
+        @lotto1 = draw_arrays[0][0].inspect
+        @lotto2 = draw_arrays[0][1].inspect
+        @lotto3 = draw_arrays[0][2].inspect
+        @lotto4 = draw_arrays[0][3].inspect
+        @lotto5 = draw_arrays[0][4].inspect
+        @lotto6 = draw_arrays[0][5].inspect
         
-        @lotto1 = (1..45).to_a.sample
         @pic1 = @lotto1.to_s + ".jpg"
-        @lotto2 = (1..45).to_a.sample
         @pic2 = @lotto2.to_s + ".jpg"
-        @lotto3 = (1..45).to_a.sample
         @pic3 = @lotto3.to_s + ".jpg"
-        @lotto4 = (1..45).to_a.sample
         @pic4 = @lotto4.to_s + ".jpg"
-        @lotto5 = (1..45).to_a.sample
         @pic5 = @lotto5.to_s + ".jpg"
-        @lotto6 = (1..45).to_a.sample
         @pic6 = @lotto6.to_s + ".jpg"
         
     end
